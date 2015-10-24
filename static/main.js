8652fe54e21579f268040165df1c7dd704891bbe
Ken Rockot <rockot@chromium.org>
2015-10-24 12:54:55 -0700

    eh

--- static/main.js
+++ static/main.js
@@ -1,15 +1,32 @@
 'use strict';
 
 let randomId = () => (Math.random() * 100000000) | 0;
-let selectChannel = (id) => { document.body.innerText = id; };
 let updateChannel = () => { selectChannel(window.location.hash.substr(1)); };
 
-window.addEventListener('hashchange', updateChannel);
-
-window.addEventListener('load', () => {
+let init = () => {
   if (window.location.hash === '#' || window.location.hash === '')
     window.location.hash = '#' + randomId();
   else
     updateChannel();
-});
+};
+
+window.addEventListener('hashchange', updateChannel);
+window.addEventListener('load', init);
+
+let selectChannel = (id) => {
+  console.log('Switching to channel ' + id);
+};
+
+let createIceService = function(urlString, username, password) {
+  let url = new URL(urlString);
+  if (url.protocol === 'stun:') {
+    return { 'url': url.toString() };
+  } else if (url.protocol === 'turn:') {
+    return {
+      'url': url.toString(),
+      'username': username,
+      'credential': password
+    };
+  }
+};
 
